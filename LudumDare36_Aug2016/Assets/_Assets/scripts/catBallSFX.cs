6515a96bfc25048bba58b76a72cbabe0e1cb97b3
tania P <chinnie@popoko.com>
2017-05-08 15:22:54 -0700

    LD 36 - VR sdk, project settings, custom assets

---
+++ LudumDare36_Aug2016/Assets/_Assets/scripts/catBallSFX.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class catBallSFX : MonoBehaviour {
+
+    public AudioClip ballSound;
+
+    private AudioSource source;
+
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+            source.PlayOneShot(ballSound, 1.0f);
+    }
+}
---
+++ LudumDare36_Aug2016/Assets/_Assets/scripts/catBallSFXCollision.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class catBallSFXCollision : MonoBehaviour {
+
+    public AudioClip ballSound;
+
+    private AudioSource source;
+
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+ /*   IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        // Code to execute after the delay
+    }
+*/
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("TriggerObject"))
+        {
+            source.PlayOneShot(ballSound, 1.0f);
+
+            Collider selfCollider = GetComponent<Collider>();
+            selfCollider.enabled = false;
+
+        }
+//        StartCoroutine(ExecuteAfterTime(10));
+//        selfCollider.enabled = true;
+    }
+
+
+
+}
